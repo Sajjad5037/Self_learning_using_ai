f566c211fd4985a744806850ed88efd2824d0f05
Sajjad Ali Noor <proactive1.san@gmail.com>
2026-05-12 12:45:31 +0500

    Update session_utils.py

--- utils/session_utils.py
+++ utils/session_utils.py
@@ -1,1 +1,21 @@
 
+import streamlit as st
+
+
+
+def initialize_session_state():
+
+    defaults = {
+        "child_name": "",
+        "child_age": 13,
+        "guardian_email": "",
+        "session_duration_minutes": 20,
+        "progress": 0,
+        "chat_history": [],
+        "current_sentence": ""
+    }
+
+    for key, value in defaults.items():
+
+        if key not in st.session_state:
+            st.session_state[key] = value
