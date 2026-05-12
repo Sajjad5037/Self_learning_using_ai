cc16c3b1bd2d68965f9244f1e41edd4df8055ff7
Sajjad Ali Noor <proactive1.san@gmail.com>
2026-05-12 12:44:00 +0500

    Update email_service.py

--- services/email_service.py
+++ services/email_service.py
@@ -1,1 +1,39 @@
 
+import smtplib
+from email.mime.text import MIMEText
+
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADDRESS = "YOUR_EMAIL@gmail.com"
+EMAIL_PASSWORD = "YOUR_APP_PASSWORD"
+
+
+
+def send_email(receiver_email, subject, body):
+
+    msg = MIMEText(body)
+
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = receiver_email
+
+    server = smtplib.SMTP(
+        SMTP_SERVER,
+        SMTP_PORT
+    )
+
+    server.starttls()
+
+    server.login(
+        EMAIL_ADDRESS,
+        EMAIL_PASSWORD
+    )
+
+    server.sendmail(
+        EMAIL_ADDRESS,
+        receiver_email,
+        msg.as_string()
+    )
+
+    server.quit()
