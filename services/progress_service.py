246be1e9e9e7ecb4c29283c051688be6089530ab
Sajjad Ali Noor <proactive1.san@gmail.com>
2026-05-12 12:44:44 +0500

    Update progress_service.py

--- services/progress_service.py
+++ services/progress_service.py
@@ -1,1 +1,11 @@
 
+import streamlit as st
+
+
+
+def update_progress(progress_increment):
+
+    st.session_state.progress += progress_increment
+
+    if st.session_state.progress > 100:
+        st.session_state.progress = 100
